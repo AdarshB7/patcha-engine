9e826f155905f2b00b6ac7aaa682ff7ca8f290aa
v4nshSharma <v4nsh.sharma@gmail.com>
2025-04-19 14:29:45 -0700

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="patcha",
-    version="0.2.0",
+    version="0.2.2",
     license="MIT",
     packages=find_packages(),
     install_requires=[
